8448e4689399794474bf93b7bdf97b45ca17c278
Abel Sotillos <abelsotillos@gmail.com>
2025-06-10 22:54:00 +0200

    cambiado fuente y cosas login

--- src/main/webapp/app/layouts/main/main.component.ts
+++ src/main/webapp/app/layouts/main/main.component.ts
@@ -5,12 +5,13 @@ import { AccountService } from 'app/core/auth/account.service';
 import { AppPageTitleStrategy } from 'app/app-page-title-strategy';
 import FooterComponent from '../footer/footer.component';
 import PageRibbonComponent from '../profiles/page-ribbon.component';
+import SharedModule from 'app/shared/shared.module';
 
 @Component({
   selector: 'jhi-main',
   templateUrl: './main.component.html',
   providers: [AppPageTitleStrategy],
-  imports: [RouterOutlet, FooterComponent, PageRibbonComponent],
+  imports: [RouterOutlet, FooterComponent, PageRibbonComponent, SharedModule],
 })
 export default class MainComponent implements OnInit {
   isLoggin = true;
@@ -31,7 +32,7 @@ export default class MainComponent implements OnInit {
     });
     this.router.events.subscribe({
       next: () => {
-        if (this.router.url === '/login') {
+        if (this.router.url === '/login' || this.router.url === '/account/register' || this.router.url.includes('/account/reset')) {
           this.isLoggin = true;
         } else {
           this.isLoggin = false;
