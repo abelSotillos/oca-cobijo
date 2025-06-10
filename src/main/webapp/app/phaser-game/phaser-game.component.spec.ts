55d721a32074eee5d3d7f67379055cb8e487a0f8
Abel Sotillos Cuenca <53355973+abelSotillos@users.noreply.github.com>
2025-06-10 22:51:48 +0200

    feat(websocket): broadcast game changes

--- src/main/webapp/app/phaser-game/phaser-game.component.spec.ts
+++ src/main/webapp/app/phaser-game/phaser-game.component.spec.ts
@@ -1,5 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+jest.mock('phaser', () => ({
+  Game: jest.fn().mockImplementation(() => ({ destroy: jest.fn() })),
+  Scene: class {},
+  AUTO: 0,
+  GameObjects: { Image: class {} },
+}));
+
 import { PhaserGameComponent } from './phaser-game.component';
 
 describe('PhaserGameComponent', () => {
